c09e3abfc54197ca484bd13ae1e4b557b4d2ecfd
kernelk14 <khyleisaias@gmail.com>
2024-08-16 12:09:45 +0000

    Get Details about a specific location

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import locationiq from "@api/locationiq"
-//import prompt from "prompt-sync"
-// const { GetLatLngByAddress } = require('@geocoder-free/google')
 
-// let coord = []
-
-//const coord = prompt("Search for a place: ")
+const { exec } = require('child_process')
+const request = require('request')
 const coord = "Silang Cavite"
 var latitude = ""
 var longitude = ""
+
+var lat_stack = []
+var lon_stack = []
+
 
 locationiq.auth('pk.d62018581d60599110ccf60633c9c34a');
 locationiq.search({
@@ -20,11 +21,16 @@ locationiq.search({
       
       latitude = data['0']['lat']
       longitude = data['0']['lon']
-      console.log(latitude)
-      console.log(longitude)
+      lat_stack.push(latitude)
+      lon_stack.push(longitude)
+      request(`https://api.pirateweather.net/forecast/F9P7KSO47e0vNCwgXaISC9meIBg5k3TA/${latitude},${longitude}`, (error, response, body) => {
+          if (error) {
+              console.error(`error: ${error}`)
+              return
+          }
+          console.log(body)
+      })
+      //console.log(latitude)
+      //console.log(longitude)
   })
   .catch(err => console.error(err));
-
-// const req = GetLatLngByAddress("Silang, Cavite").then(console.log)
-
-// console.log(data)
